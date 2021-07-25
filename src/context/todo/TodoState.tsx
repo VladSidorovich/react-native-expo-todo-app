c1c499e70ec56eb741b94fd9a60de44d96760f17
Vlad Sidorovich <vladsidorovi4@gmail.com>
2021-07-25 11:24:59 +0300

    created todo reducer

--- src/context/todo/TodoState.tsx
+++ src/context/todo/TodoState.tsx
@@ -1,23 +1,37 @@
 import React, { useReducer } from "react";
+import { ADD_TODO } from "../types";
 import { TodoContext } from "./todoContext";
 import { todoReducer } from "./todoReducer";
 
 export const TodoState = ({ children }: any) => {
   const initState = {
-    todos: [
-      {
-        id: "1",
-        title: "test",
-      },
-    ],
+    todos: [],
   };
 
   const [state, dispatch] = useReducer(todoReducer, initState);
+
+  const addTodo = (title: string) => dispatch({
+    type: ADD_TODO,
+    title
+  })
+
+  const removeTodo = (id: string) => dispatch({
+    type: ADD_TODO,
+    id
+  })
+
+  const updateTodo = (title: string) => dispatch({
+    type: ADD_TODO,
+    title
+  })
 
   return (
     <TodoContext.Provider
       value={{
         todos: state.todos,
+        addTodo,
+        removeTodo,
+        updateTodo
       }}
     >
       {children}
